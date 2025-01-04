7d86bef84dd581a55c5727c29a7cea4d3c667ab3
Manojna <manojna.vinjamuri@iongroup.com>
2025-01-04 17:00:07 +0530

    add readme.md, update the skeleton of the project with no style

---
+++ src/pages/hackathon/[name].tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router"
+
+const Hackathon: React.FC<{}> = () => {
+    const router = useRouter()
+    
+    return (
+        <div>
+            <h1>Hackathon: {router.query.name}</h1>
+        </div>
+    )
+}
+
+export default Hackathon
